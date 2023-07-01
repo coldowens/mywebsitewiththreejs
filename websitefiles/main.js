84708a633cb663d696046c10dff0f5cd13a9871e
Travis Opritza <opritza.5@gmail.com>
2023-06-30 22:04:06 -0400

    added basic non-rotating torus and verified working

--- websitefiles/main.js
+++ websitefiles/main.js
@@ -1,19 +1,27 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+
 import * as THREE from 'three';
 
 const scene = new THREE.Scene();
 
+
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer({
-     canvas: document.querySelector('#bg'),
+  canvas: document.querySelector('#bg'),
 });
 
 renderer.setPixelRatio( window.devicePixelRatio );
-renderer.setSize ( window.innerWidth, window.innerHeight );
+renderer.setSize( window.innerWidth, window.innerHeight );
 camera.position.setZ(30);
+
+renderer.render( scene, camera);
+
+const geometry = new THREE.TorusGeometry( 10, 3, 16, 100)
+const material = new THREE.MeshBasicMaterial( { color: 0xFF6347, wireframe: true } );
+const torus = new THREE.Mesh( geometry, material );
+
+
+scene.add(torus)
 
 renderer.render( scene, camera );
